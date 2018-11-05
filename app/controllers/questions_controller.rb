8fec9e57bf41a67894ca7fcbd925794cada9a89d
Jessie <jessie.baxter29@gmail.com>
2018-11-05 17:10:12 +1100

    Added testing to Stupid Coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,10 +14,3 @@ class QuestionsController < ApplicationController
 		end
 	end
 end
-
-
-# LOGIC
-# If the message is I am going to work, the coach will answer Great!
-# If the message has a question mark ? at the end, the coach will 
-# answer Silly question, get dressed and go to work!.
-# Otherwise he will answer I don't care, get dressed and go to work!
--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -20,8 +20,34 @@ class QuestionsTest < ApplicationSystemTestCase
   end
 
   test "clicking 'Ask a new question' takes you back to the ask page" do
-  	visit answer_url
+  	visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
   	click_on "Ask a new question"
-  	assert_selector "p", text: "Ask your coach anything"
+  	assert_text "Ask your coach anything"
+  end
+
+  test "asking a question, gets a 'silly question' response" do 
+    visit ask_url
+    fill_in "question", with: "Hello?"
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
+  end
+
+  test "telling coach you are going to work, gets a positive response" do    
+    visit ask_url
+    fill_in "question", with: "I am going to work"
+    click_on "Ask"
+
+    assert_text "Great!"
+  end
+
+  test "asking nothing, gets a grumpy response" do    
+    visit ask_url
+    fill_in "question", with: ""
+    click_on "Ask"
+
+    assert_text "So, do you have a question for me or what?"
   end
 end
